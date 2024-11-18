19500679005e242d5d1aa437e4ca67961bd8eec7
Iskandarbek <iskandarattak@gmail.com>
2024-11-18 10:54:12 +0500

    Restart-app

---
+++ app/src/main/java/uz/iskandarbek/rebot/MainActivity.kt
@@ -0,0 +1,25 @@
+package uz.iskandarbek.rebot
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        btn = findViewById(R.id.btn)
+        btn.setOnClickListener {
+            val intent = packageManager.getLaunchIntentForPackage(packageName)
+            intent?.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finishAffinity() // Ilovani tozalab ochish
+        }
+    }
+}
